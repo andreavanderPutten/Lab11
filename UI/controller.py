324705a4fa11b30ff1b7ecc8ff042d5c4253bc9f
chiara vander <s264255@studenti.polito.it>
2024-07-09 16:50:48 +0200

    finito

--- UI/controller.py
+++ UI/controller.py
@@ -24,30 +24,41 @@ class Controller:
         anno = self._view._ddyear.value
         colore = self._view._ddcolor.value
 
+
+
         if  anno == None or anno == None:
             self._view.create_alert("Non hai scelto un valore di colore o di anno!!")
             return
         else :
             self._model.creaGrafo(colore,anno)
             self._view.txtOut.controls.append(ft.Text(f"Nodi: {self._model.grafoDetails()[0]},Archi: {self._model.grafoDetails()[1]}"))
+            self.fillDDProduct()
             self._view.update_page()
-        top3 = self._model.top3Archi()
+        top3 = self._model.get_sorted_edges()[:3]
         lista_nodi = {}
         for arco in top3 :
-            self._view.txtOut.controls.append(ft.Text(f"{arco[0]}<->{arco[1]}"))
-            if arco[0].Prdouct_number not in lista_nodi :
+            self._view.txtOut.controls.append(ft.Text(f"{arco[0].Product_number}<->{arco[1].Product_number} peso :{arco[2]["weight"]}"))
+            if arco[0].Product_number not in lista_nodi :
                 lista_nodi[arco[0].Product_number] = 1
             else:
                 lista_nodi[arco[0].Product_number] += 1
 
-            if arco[1].Prdouct_number not in lista_nodi :
+            if arco[1].Product_number not in lista_nodi :
                 lista_nodi[arco[1].Product_number] = 1
             else:
                 lista_nodi[arco[1].Product_number] += 1
+            n_repeated = [k for (k,v) in lista_nodi.items() if v > 1]
+        self._view.txtOut.controls.append(ft.Text(f"i nodi ripetuti sono : {n_repeated}"))
+        self._view.update_page()
 
-
-    def handle_search(self):
-        pass
+    def fillDDProduct(self):
+        for n in self._model.get_nodes():
+            self._view._ddnode.options.append(ft.dropdown.Option(n.Product_number))
+        self._view.update_page()
+    def handle_search(self,e):
+        self._model.trova_cammino(int(self._view._ddnode.value))
+        self._view.txtOut2.controls.append(ft.Text(f"Numero di archi del cammino massimo : {len(self._model.solBest)}"))
+        self._view.update_page()
 
 
 
--- model/model.py
+++ model/model.py
@@ -8,6 +8,8 @@ from database.DAO import DAO
 class Model :
     def __init__(self):
         self.grafo = nx.Graph()
+        self.idMap = {}
+        self.solBest = []
     def creaGrafo(self,colore,anno):
         self.grafo.clear()
         nodi = DAO.getVertci()
@@ -15,6 +17,7 @@ class Model :
         for nodo in nodi :
             if nodo.Product_color == colore :
                 nodi_veri.append(nodo)
+                self.idMap[nodo.Product_number] = nodo
         self.grafo.add_nodes_from(nodi_veri)
 
         for nodo1 in nodi_veri :
@@ -29,12 +32,60 @@ class Model :
 
     def grafoDetails(self):
         return len(self.grafo.nodes), len(self.grafo.edges)
-    def top3Archi(self):
-        archi_ordinati = copy.deepcopy(self.grafo.edges)
-        sorted(archi_ordinati,key= lambda nodo : nodo[2])
-        top3 = archi_ordinati[:3]
-        return top3
 
+    def get_sorted_edges(self):
+        return sorted(self.grafo.edges(data=True), key=lambda x: x[2]['weight'], reverse=True)
+    def trova_cammino(self,numero_prodotto):
+        nodo_iniziale = self.idMap[numero_prodotto]
+
+        parziale = []
+
+        self.ricorsione(parziale,nodo_iniziale,0)
+
+        print("final",len(self.solBest),[i[2]["weight"] for i in self.solBest])
+    def ricorsione(self,parziale,nodo_ultimo,livello):
+        archiViciniAmmissibili = self.getArchiViciniAmm(nodo_ultimo,parziale)
+
+        if len(archiViciniAmmissibili) == 0:
+            if len(parziale) > len(self.solBest) :
+                self.solBest = list(parziale)
+                print(len(self.solBest), [ii[2]["weight"] for ii in self.solBest])
+        for a in archiViciniAmmissibili :
+            parziale.append(a)
+            self.ricorsione(parziale,a[1],livello+1)
+            parziale.pop()
+
+    def getArchiViciniAmm(self,nodo_ultimo,parziale):
+        archiVicini = self.grafo.edges(nodo_ultimo, data=True)
+        result = []
+        for a  in archiVicini :
+            if self.isAscendent(a,parziale) and self.isNovel(a,parziale) :
+                result.append(a)
+        return result
+    def isAscendent(self,e,parziale):
+        if len(parziale) == 0 :
+            print("parziale id empty in ascendent")
+            return True
+        return e[2]["weight"] >= parziale[-1][2]["weight"]
+    def isNovel(self, e, parziale):
+        if len(parziale)==0:
+            print("parziale is empty in isnovel")
+            return True
+        #faccio entrambi i casi
+        e_inv = (e[1], e[0], e[2])
+        return (e_inv not in parziale) and (e not in parziale)
+
+    def get_nodes(self):
+        return self.grafo.nodes()
+
+    def get_edges(self):
+        return list(self.grafo.edges(data=True))
+
+    def get_num_of_nodes(self):
+        return self.grafo.number_of_nodes()
+
+    def get_num_of_edges(self):
+        return self.grafo.number_of_edges()
 
 
 
